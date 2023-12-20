dafa4e56893d6b718871df4039e0d51f3bbd1c71
TerrificTable <omegagaming189@gmail.com>
2023-12-20 14:49:09 +0100

    some headers with simple stuff that makes your life a little easier

---
+++ include/lib/types.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+typedef unsigned char byte;
+
+typedef _Bool bool;
+#define true 1
+#define false 0
